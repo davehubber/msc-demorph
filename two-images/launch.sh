8ac94938604ecc7d43586db0f4644431f33f0782
David Cordeiro <up202108820@g.uporto.pt>
2025-12-27 21:49:40 +0000

    Add launch file

---
+++ two-images/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#SBATCH --partition=gpu_min12gb_ext   # Partition where the job will be run. Check with "$ sinfo".
+#SBATCH --qos=gpu_min12gb_ext     # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
+#SBATCH --job-name=demorph_test   # Job name
+#SBATCH --output=slurm_%x.%j.out  # File containing STDOUT output
+#SBATCH --error=slurm_%x.%j.err   # File containing STDERR output. If ommited, use STDOUT.
+
+# Commands / scripts to run (e.g., python3 train.py)
+
+python train.py --run_name "demorph_test" --dataset_path "/nas-ctm01/datasets/public/Oxford102Flowers/jpg/" --partition_file "partition.csv" --epochs 800
