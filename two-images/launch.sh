15d8d72499b0a04fa363426625948009c71db1bc
David Cordeiro <up202108820@g.uporto.pt>
2026-01-23 18:06:20 +0000

    Update gpu partition

--- two-images/launch.sh
+++ two-images/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH --partition=gpu_min32gb_ext   # Partition where the job will be run. Check with "$ sinfo".
-#SBATCH --qos=gpu_min32gb_ext     # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
+#SBATCH --partition=gpu_min32gb   # Partition where the job will be run. Check with "$ sinfo".
+#SBATCH --qos=gpu_min32gb     # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
 #SBATCH --job-name=demorph_test_2   # Job name
 #SBATCH --output=slurm_%x.%j.out  # File containing STDOUT output
 #SBATCH --error=slurm_%x.%j.err   # File containing STDERR output. If ommited, use STDOUT.
