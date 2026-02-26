af4df9e7c29318409bba0b4e1c381bcdd90eb1e8
David Cordeiro <up202108820@g.uporto.pt>
2026-02-26 11:16:55 +0000

    Change launch file partition

--- two-images/launch.sh
+++ two-images/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH --partition=gpu_min24gb_ext   # Partition where the job will be run. Check with "$ sinfo".
-#SBATCH --qos=gpu_min24gb_ext     # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
+#SBATCH --partition=gpu_min11gb_ext   # Partition where the job will be run. Check with "$ sinfo".
+#SBATCH --qos=gpu_min11gb_ext     # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
 #SBATCH --job-name=dual_prediction   # Job name
 #SBATCH --output=slurm_%x.%j.out  # File containing STDOUT output
 #SBATCH --error=slurm_%x.%j.err   # File containing STDERR output. If ommited, use STDOUT.
