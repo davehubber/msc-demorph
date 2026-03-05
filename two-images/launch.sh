f0326a8564b59dae4addf348f71e05751cacee57
David Cordeiro <up202108820@g.uporto.pt>
2026-03-05 21:10:21 +0000

    Change launch file

--- two-images/launch.sh
+++ two-images/launch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-#SBATCH --partition=gpu_min24gb_ext  # Partition where the job will be run. Check with "$ sinfo".
-#SBATCH --qos=gpu_min24gb_ext        # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
+#SBATCH --partition=gpu_min12gb_ext  # Partition where the job will be run. Check with "$ sinfo".
+#SBATCH --qos=gpu_min12gb_ext        # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
 #SBATCH --job-name=baseline          # Job name
 #SBATCH --output=slurm_%x.%j.out     # File containing STDOUT output
 #SBATCH --error=slurm_%x.%j.err      # File containing STDERR output. If ommited, use STDOUT.
 
 # Commands / scripts to run (e.g., python3 train.py)
 
-python train.py --dataset_path "/nas-ctm01/datasets/public/Oxford102Flowers/jpg" --run_name "baseline" --partition_file "partition.csv" --batch_size 512
+python train.py --dataset_path "/nas-ctm01/datasets/public/Oxford102Flowers/jpg" --run_name "baseline" --partition_file "partition.csv" --batch_size 256
