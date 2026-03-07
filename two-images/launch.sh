4c5df6d24d9d2d7b2622f1904fae4c8bf922ba41
David Cordeiro <up202108820@g.uporto.pt>
2026-03-07 12:34:09 +0000

    Save results

--- two-images/launch.sh
+++ two-images/launch.sh
@@ -8,4 +8,4 @@
 
 # Commands / scripts to run (e.g., python3 train.py)
 
-python train.py --dataset_path "/nas-ctm01/datasets/public/Oxford102Flowers/jpg" --run_name "faster_baseline_2" --partition_file "partition.csv" --sampling_method "cold_diffusion" --batch_size 512 --epochs 750
+python train.py --dataset_path "/nas-ctm01/datasets/public/Oxford102Flowers/jpg" --run_name "faster_baseline_2" --partition_file "partition.csv" --sampling_method "cold_diffusion" --batch_size 128 --epochs 750
