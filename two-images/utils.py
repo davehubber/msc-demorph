f40b261059592648c584ff501ade34265e385597
David Cordeiro <up202108820@g.uporto.pt>
2026-03-16 11:07:13 +0000

    Increase num_workers

--- two-images/utils.py
+++ two-images/utils.py
@@ -68,7 +68,7 @@ def get_data(args, partition):
     shuffle = True
     if partition == 'test':
         shuffle = False
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=4, pin_memory=True)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=16, pin_memory=True)
     return dataloader
 
 def setup_logging(run_name):
